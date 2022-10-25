23d35a42bb228e64f3a06f32e8d954b66e442a49
Sergio-hdD <sergio-hdd@hotmail.com>
2022-10-24 22:49:20 -0300

    Adaptación básica de las 3 hijas de User

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -35,6 +35,7 @@ class StudentController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $student->setRoles(["ROLE_STUDENT"]);
             $studentRepository->add($student, true);
 
             return $this->redirectToRoute('app_student_index', [], Response::HTTP_SEE_OTHER);
